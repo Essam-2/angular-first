b841783c79046ed9f5a2c99e4c0e09a9bb5f8071
Essam <esamanwar2@gmail.com>
2023-12-10 01:11:40 +0300

    [-] Remove Footer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent, FooterComponent, TopHeaderComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
