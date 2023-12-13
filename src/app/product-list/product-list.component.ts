3da962ed13956cc24619bcbc50d2f09b160ef2ac
Essam <esamanwar2@gmail.com>
2023-12-14 01:27:58 +0300

    [+] example og ngFor

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,42 +1,14 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, NgFor, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  // name = 'John Doe';
-  addToCart: number = 0;
-  product = {
-    name: 'iPhone X',
-    price: 999,
-    color: 'Red',
-    discount: 8.2,
-    inStock: 5,
-    PImage: '/assets/images/iphone.png',
-  };
-
-  getDiscountedPrice() {
-    return (
-      this.product.price - (this.product.price * this.product.discount) / 100
-    );
-  }
-
-  decrementCartValue() {
-    if (this.addToCart === 0) return;
-    this.addToCart--;
-  }
-
-  incrementCartValue() {
-    if (this.addToCart >= this.product.inStock) return;
-    this.addToCart++;
-  }
-
-  // onNameChange(event: any) {
-  //   this.name = event.target.value;
-  // }
+  listOfString: string[] = ['Mark', ' Steve'];
 }
