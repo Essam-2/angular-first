14bb6660bd0970ef8b7388a23763f7e5bd035051
Essam <esamanwar2@gmail.com>
2023-12-14 00:43:44 +0300

    [+] add productList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent, TopHeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    TopHeaderComponent,
+    ProductListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-first';
-  address = "Today is Thursday"
+  address = 'Today is Thursday';
 }
