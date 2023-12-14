43da0b64341a10d1de124814dc263144d1ba0e0d
Essam <esamanwar2@gmail.com>
2023-12-14 23:21:02 +0300

    [+] add some specific style using ngClass

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,16 +1,16 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, NgClass],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent {
-  searchText: string = 'Mens wear';
+  searchText: string = '';
 
   updateSearchText(event: any) {
     this.searchText = event.target.value;
