ad31e4bdd104320e49176a2f9fc17142be7c07ed
Essam <esamanwar2@gmail.com>
2024-01-17 20:02:33 +0300

    [+] use Product and viewChild

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SearchComponent } from './search/search.component';
 import { CommonModule, NgFor } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [SearchComponent, NgFor, CommonModule, ProductListComponent],
+  imports: [
+    SearchComponent,
+    NgFor,
+    CommonModule,
+    ProductListComponent,
+    ProductDetailComponent,
+  ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
 export class ContainerComponent {
   listOfString: string[] = ['Mark', ' Steve'];
   searchText: string = '';
+
+  @ViewChild(ProductListComponent) productListComponent: ProductListComponent;
 
   setSearchText(value: string) {
     this.searchText = value;
